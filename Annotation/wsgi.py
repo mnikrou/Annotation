2e46bacf8eb5e282fa1e00d3354939c6c3b3d476
mnikrou <mi.nikrou@gmail.com>
2017-11-28 00:10:57 +0200

    c

--- Annotation/wsgi.py
+++ Annotation/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Annotatio.configs.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Annotation.configs.settings")
 
 application = get_wsgi_application()
